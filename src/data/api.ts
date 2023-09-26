07c86ee094717f90ab2e2ae951e7a24cccf6a89c
Iryna Rudzko <irinrudko@gmail.com>
2023-09-26 20:11:34 +0200

    feat: fetch transactions data

---
+++ src/data/api.ts
@@ -0,0 +1,17 @@
+import { Transaction, User } from '../types/types'
+
+const JSON_FILE_PATH = '/data/bankData.json'
+
+export async function fetchUsersAndTransactions(): Promise<{ users: User[]; transactions: Transaction[] }> {
+    try {
+        const response = await fetch(JSON_FILE_PATH)
+        if (!response.ok) {
+            throw new Error('Failed to fetch JSON data')
+        }
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error('Error fetching JSON data:', error)
+        throw error
+    }
+}
