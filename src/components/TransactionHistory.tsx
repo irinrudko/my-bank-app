2be4ad7020730b5dab715b9711f39c8e2d753576
Iryna Rudzko <irinrudko@gmail.com>
2023-09-27 11:53:48 +0200

    refactor: create one table

--- src/components/TransactionHistory.tsx
+++ src/components/TransactionHistory.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Transaction, User } from '../types/types'
 
 type TransactionHistoryProps = {
@@ -14,41 +15,66 @@ export const TransactionHistory: React.FC<TransactionHistoryProps> = ({ transact
         ? transactions.filter((transaction) => transaction.sourceId === selectedUser.id)
         : []
 
+    const calculateUserBalance = () => {
+        const incomingTotal = incomingTransactions.reduce((total, transaction) => total + transaction.amount, 0)
+        const outgoingTotal = outgoingTransactions.reduce((total, transaction) => total + transaction.amount, 0)
+        return incomingTotal - outgoingTotal
+    }
+
+    const userBalance = selectedUser ? calculateUserBalance() : 0
+
     return (
         <div>
-            <h2>Transaction History</h2>
-            <h3>Incoming Transactions</h3>
+            <h2>Account Overview</h2>
             <table>
+                <caption>{selectedUser?.name}</caption>
                 <thead>
                     <tr>
-                        <th>Source ID</th>
-                        <th>Amount</th>
+                        <th scope="col">Balance</th>
+                        <th scope="col">Incoming Transactions</th>
+                        <th scope="col">Outgoing Transactions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {incomingTransactions.map((transaction, index) => (
-                        <tr key={index}>
-                            <td>{transaction.sourceId}</td>
-                            <td>{transaction.amount.toFixed(2)}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <h3>Outgoing Transactions</h3>
-            <table>
-                <thead>
                     <tr>
-                        <th>Target ID</th>
-                        <th>Amount</th>
+                        <td>{userBalance.toFixed(2)}</td>
+                        <td>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Source ID</th>
+                                        <th scope="row">Amount</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {incomingTransactions.map((transaction, index) => (
+                                        <tr key={index}>
+                                            <td>{transaction.sourceId}</td>
+                                            <td>{transaction.amount.toFixed(2)}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Target ID</th>
+                                        <th scope="col">Amount</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {outgoingTransactions.map((transaction, index) => (
+                                        <tr key={index}>
+                                            <td>{transaction.targetId}</td>
+                                            <td>{transaction.amount.toFixed(2)}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    {outgoingTransactions.map((transaction, index) => (
-                        <tr key={index}>
-                            <td>{transaction.targetId}</td>
-                            <td>{transaction.amount.toFixed(2)}</td>
-                        </tr>
-                    ))}
                 </tbody>
             </table>
         </div>
