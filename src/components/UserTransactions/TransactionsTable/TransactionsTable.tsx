3fda8df476f9f00542eaff2f221b51db8bf44247
Iryna Rudzko <irinrudko@gmail.com>
2023-10-02 16:38:53 +0200

    feat: add pagination

--- src/components/UserTransactions/TransactionsTable/TransactionsTable.tsx
+++ src/components/UserTransactions/TransactionsTable/TransactionsTable.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Transaction, User } from '../../../types/types'
 import { TransactionTable } from './TransactionTable/TransactionTable'
 import common from '../../../styles/common.module.scss'
+import styles from './TransactionsTable.module.scss'
 
 type TransactionsTableType = {
     transactions: Transaction[]
@@ -14,6 +15,8 @@ type SortOrderType = 'asc' | 'desc'
 export const TransactionsTable: React.FC<TransactionsTableType> = ({ transactions, selectedUser }) => {
     const [incomingSortOrder, setIncomingSortOrder] = useState<SortOrderType>('asc')
     const [outgoingSortOrder, setOutgoingSortOrder] = useState<SortOrderType>('asc')
+    const [currentPage, setCurrentPage] = useState<number>(1)
+    const [itemsPerPage] = useState<number>(20)
 
     const toggleSortOrder = (column: TransactionType) => {
         switch (column) {
@@ -27,39 +30,127 @@ export const TransactionsTable: React.FC<TransactionsTableType> = ({ transaction
                 break
         }
     }
+
+    const sortTransactions = (column: TransactionType): Transaction[] => {
+        const sortedTransactions = [...transactions]
+
+        switch (column) {
+            case 'incoming':
+                if (incomingSortOrder === 'asc') {
+                    sortedTransactions.sort((a, b) => a.amount - b.amount)
+                } else {
+                    sortedTransactions.sort((a, b) => b.amount - a.amount)
+                }
+                break
+            case 'outgoing':
+                if (outgoingSortOrder === 'asc') {
+                    sortedTransactions.sort((a, b) => a.amount - b.amount)
+                } else {
+                    sortedTransactions.sort((a, b) => b.amount - a.amount)
+                }
+                break
+            default:
+                break
+        }
+
+        return sortedTransactions
+    }
+
+    //pagination
+    const filteredIncomingTransactions = transactions.filter((transaction) => transaction.targetId === selectedUser?.id)
+    const filteredOutgoingTransactions = transactions.filter((transaction) => transaction.sourceId === selectedUser?.id)
+
+    const totalItems = Math.max(filteredIncomingTransactions.length, filteredOutgoingTransactions.length)
+    const totalPages = Math.ceil(totalItems / itemsPerPage)
+
+    const startIndex = (currentPage - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+
+    const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1)
+
+    const goToPage = (page: number) => {
+        if (page >= 1 && page <= totalPages) {
+            setCurrentPage(page)
+        }
+    }
+
+    let startPage = Math.max(1, currentPage - 4)
+    let endPage = Math.min(startPage + 9, totalPages)
+
+    if (endPage - startPage < 9) {
+        endPage = Math.min(totalPages, startPage + 9)
+        startPage = Math.max(1, endPage - 9)
+    }
+    //pagination
+
     return (
-        <table className={common.table}>
-            <caption>{selectedUser?.name}</caption>
-            <thead>
-                <tr>
-                    <th>Incoming Transactions</th>
-                    <th>Outgoing Transactions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <TransactionTable
-                            transactions={transactions.filter(
-                                (transaction) => transaction.targetId === selectedUser?.id
-                            )}
-                            sortOrder={incomingSortOrder}
-                            onSortOrderChange={() => toggleSortOrder('incoming')}
-                            type="incoming"
-                        />
-                    </td>
-                    <td>
-                        <TransactionTable
-                            transactions={transactions.filter(
-                                (transaction) => transaction.sourceId === selectedUser?.id
-                            )}
-                            sortOrder={outgoingSortOrder}
-                            onSortOrderChange={() => toggleSortOrder('outgoing')}
-                            type="outgoing"
-                        />
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div>
+            <table className={common.table}>
+                <caption>{selectedUser?.name}</caption>
+                <thead>
+                    <tr>
+                        <th>Incoming Transactions</th>
+                        <th>Outgoing Transactions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <TransactionTable
+                                transactions={sortTransactions('incoming').slice(startIndex, endIndex)}
+                                sortOrder={incomingSortOrder}
+                                onSortOrderChange={() => toggleSortOrder('incoming')}
+                                type="incoming"
+                            />
+                        </td>
+                        <td>
+                            <TransactionTable
+                                transactions={sortTransactions('outgoing').slice(startIndex, endIndex)}
+                                sortOrder={outgoingSortOrder}
+                                onSortOrderChange={() => toggleSortOrder('outgoing')}
+                                type="outgoing"
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            {/* Pagination */}
+            <div className={styles.pagination}>
+                <button className={styles.button} onClick={() => goToPage(1)} disabled={currentPage === 1}>
+                    First Page
+                </button>
+                <button
+                    className={styles.button}
+                    onClick={() => goToPage(currentPage - 1)}
+                    disabled={currentPage === 1}
+                >
+                    Previous
+                </button>
+                {pageNumbers.slice(startPage - 1, endPage).map((pageNumber) => (
+                    <button
+                        key={pageNumber}
+                        onClick={() => goToPage(pageNumber)}
+                        className={`${styles.button} ${pageNumber === currentPage ? 'active' : ''}`}
+                        disabled={pageNumber === currentPage}
+                    >
+                        {pageNumber}
+                    </button>
+                ))}
+                <button
+                    className={styles.button}
+                    onClick={() => goToPage(currentPage + 1)}
+                    disabled={currentPage === totalPages}
+                >
+                    Next
+                </button>
+                <button
+                    className={styles.button}
+                    onClick={() => goToPage(totalPages)}
+                    disabled={currentPage === totalPages}
+                >
+                    Last Page
+                </button>
+            </div>
+        </div>
     )
 }
